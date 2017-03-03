9c1f823028ad783897b5b7480b63a28cef779160
Yoshihiro Tanaka <hirotnkg@gmail.com>
2017-03-02 20:59:55 -0800

    Add parity check

--- binary-string-transmitter.hs
+++ binary-string-transmitter.hs
@@ -16,14 +16,18 @@ make8 :: [Bit] -> [Bit]
 make8 bits = take 8 (bits ++ repeat 0)
 
 encode :: String -> [Bit]
-encode = concat . map (make8 . int2bin . ord)
+encode = concat . map (addparity . make8 . int2bin . ord)
 
 chop8 :: [Bit] -> [[Bit]]
 chop8 [] = []
 chop8 bits = take 8 bits : chop8 (drop 8 bits)
 
+chop9 :: [Bit] -> [[Bit]]
+chop9 [] = []
+chop9 bits = take 9 bits : chop9 (drop 9 bits)
+
 decode :: [Bit] -> String
-decode = map (chr . bin2int) . chop8
+decode = map (chr . bin2int . checkparity) . chop9
 
 transmit :: String -> String
 transmit = decode . channel . encode
@@ -31,3 +35,16 @@ transmit = decode . channel . encode
 channel :: [Bit] -> [Bit]
 channel = id
 
+addparity :: [Bit] -> [Bit]
+addparity bits = p:bits where
+                 p = parity bits
+
+checkparity :: [Bit] -> [Bit]
+checkparity (p:bits) | p == parity bits = bits
+                     | otherwise = error "parity error"
+
+parity :: [Bit] -> Int
+parity bits = (sum bits) `mod` 2
+
+channel2 :: [Bit] -> [Bit]
+channel2 = tail
