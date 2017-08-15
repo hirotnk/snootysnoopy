b8445574e48db1ab09766df0fda11127484e0ce6
Yoshihiro Tanaka <hirotnkg@gmail.com>
2017-08-14 22:59:46 -0700

    Another fib

--- misc.hs
+++ misc.hs
@@ -8,3 +8,6 @@ perfect n = [x | x <- [1..n], sum (factor x) == x]
 fibs :: [Integer]
 fibs = 0:1:zipWith (+) fibs (tail fibs)
 
+fib :: Integer -> Integer
+fib n = fst (fib2 n) where fib2 n = if n == 0 then (0,1) else (b,a+b) where (a,b) = fib2 (n-1)
+
