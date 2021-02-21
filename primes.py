0e3511b5a22ffed0d02e55f44a515a57c332d8ae
Yoshihiro Tanaka <hirotnkg@gmail.com>
2021-02-20 22:35:32 -0800

    lazy prime number generator

--- primes.py
+++ primes.py
@@ -1,4 +1,6 @@
-from itertools import count
+from itertools import count, islice, takewhile
+from math import sqrt
+
 
 def primes(n):
     return [
@@ -15,3 +17,24 @@ def factor(n):
 
 def is_prime(n):
     return factor(n) == [1]
+
+
+def is_prime2(candidate):
+    if candidate == 2:
+        return True
+    if candidate % 2 == 0:
+        return False
+    half_val = int(sqrt(candidate) + 1)
+    for x in range(3, half_val, 2):
+        if candidate % x == 0:
+            return False
+    return True
+
+
+def prime_gen(start, n):
+    primes_after_start = \
+        (candidate
+         for candidate in count(start)
+         if is_prime2(candidate)
+         )
+    return islice(primes_after_start, n)
